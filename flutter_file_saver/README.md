59435688274f600384a341d7497972df8a4a870c
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-05 16:01:57 +0200

    created base package

---
+++ flutter_file_saver/README.md
@@ -0,0 +1,3 @@
+# flutter_file_saver
+
+Interface to provide a way to save files on the device in Flutter.
---
+++ flutter_file_saver/lib/flutter_file_saver.dart
@@ -0,0 +1,46 @@
+import 'dart:io' as io;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_file_manager_platform_interface/flutter_file_manager_platform_interface.dart';
+import 'package:flutter_file_manager_web/flutter_file_manager_web.dart';
+
+class FlutterFileSaver {
+  static bool _manualRegistrationNeeded = true;
+
+  FlutterFileSaver._();
+
+  factory FlutterFileSaver() {
+    _singleton ??= FlutterFileSaver._();
+    return _singleton!;
+  }
+
+  static FlutterFileSaver? _singleton;
+
+  static FileManagerPlatform get _platform {
+    if (_manualRegistrationNeeded) {
+      if (kIsWeb &&
+          FileManagerPlatform.instance is FileManagerPlatformException) {
+        FileManagerPlatform.instance = FlutterFileManagerWeb();
+      } else if (!kIsWeb &&
+          FileManagerPlatform.instance is FileManagerPlatformException) {
+        if (io.Platform.isAndroid) {
+          // FileManagerPlatform.instance = FlutterFileSaverAndroid();
+        } else if (io.Platform.isIOS) {
+          // FileManagerPlatform.instance = FileManagerIOS();
+        }
+      }
+      _manualRegistrationNeeded = false;
+    }
+    return FileManagerPlatform.instance;
+  }
+
+  /// Write [data] inside [fileName] and return the path to the file.
+  Future<String> writeFileAsString({
+    required String fileName,
+    required String data,
+  }) =>
+      _platform.writeFileAsString(
+        fileName: fileName,
+        data: data,
+      );
+}
