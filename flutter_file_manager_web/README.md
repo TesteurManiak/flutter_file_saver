5ea91e86a5141f5cc6caf4d6cc89522984bab27b
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-05 15:19:50 +0200

    web plugin ready

--- flutter_file_manager_web/README.md
+++ flutter_file_manager_web/README.md
@@ -1,15 +1,3 @@
 # flutter_file_manager_web
 
-Web platform implementation of flutter_file_manager
-
-## Getting Started
-
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
-
-For help getting started with Flutter development, view the
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
+Web platform implementation of flutter_file_manager.
--- flutter_file_manager_web/lib/flutter_file_manager_web.dart
+++ flutter_file_manager_web/lib/flutter_file_manager_web.dart
@@ -1,8 +1,57 @@
+import 'dart:convert';
+import 'dart:html' as html;
+import 'dart:typed_data';
 
-import 'flutter_file_manager_web_platform_interface.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_file_manager_platform_interface/flutter_file_manager_platform_interface.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-class FlutterFileManagerWeb {
-  Future<String?> getPlatformVersion() {
-    return FlutterFileManagerWebPlatform.instance.getPlatformVersion();
+class FlutterFileManagerWeb extends FileManagerPlatform {
+  static void registerWith(Registrar registrar) {
+    FileManagerPlatform.instance = FlutterFileManagerWeb();
+  }
+
+  @override
+  Future<String> writeFile({
+    required String fileName,
+    required String data,
+    MimeType? type,
+  }) async {
+    final bytes = utf8.encode(data);
+    final splittedName = fileName.split('.');
+    final name = splittedName[0];
+    final mimeType = type ?? splittedName[1].toMimeType();
+
+    final downloaded = await _downloadFile(
+      bytes: Uint8List.fromList(bytes),
+      name: name,
+      type: mimeType,
+    );
+
+    return downloaded ? 'Downloads/$fileName' : '';
+  }
+
+  Future<bool> _downloadFile({
+    required Uint8List bytes,
+    required String name,
+    required MimeType type,
+  }) async {
+    try {
+      final url =
+          html.Url.createObjectUrlFromBlob(html.Blob([bytes], type.blobType));
+      final htmlDocument = html.document;
+      final anchor = htmlDocument.createElement('a') as html.AnchorElement;
+      anchor.href = url;
+      anchor.style.display = name;
+      anchor.download = name;
+      anchor.type = type.blobType;
+      html.document.body?.children.add(anchor);
+      anchor.click();
+      html.document.body?.children.remove(anchor);
+      return true;
+    } catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
   }
 }
