389862033c4b92b862c86b307556d862ba40ad6a
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-07 10:18:16 +0200

    start working on swift implementation

--- flutter_file_manager_macos/macos/Classes/FlutterFileManagerMacosPlugin.swift
+++ flutter_file_manager_macos/macos/Classes/FlutterFileManagerMacosPlugin.swift
@@ -2,18 +2,28 @@ import Cocoa
 import FlutterMacOS
 
 public class FlutterFileManagerMacosPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_file_manager_macos", binaryMessenger: registrar.messenger)
-    let instance = FlutterFileManagerMacosPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_file_manager_macos", binaryMessenger: registrar.messenger)
+        let instance = FlutterFileManagerMacosPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "writeFile":
+            handleWriteFile(result, call)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    func handleWriteFile(_ result: FlutterResult, _ call: FlutterMethodCall) {
+        if let args = call.arguments as? Dictionary<String, Any>,
+           let name = args["name"] as? String,
+           let bytes = args["bytes"] as? FlutterStandardTypedData(bytes: Data) {
+            result(name)
+        } else {
+            result(FlutterError.init(code: "Bad arguments", message: nil, details: nil))
+        }
+    }
 }
