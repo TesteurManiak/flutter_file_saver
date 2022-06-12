451a0b311e0795437edd2d1f6f4f555284400b92
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-13 01:31:59 +0200

    added melos coverage script

--- packages/flutter_file_saver/test/flutter_file_saver_test.dart
+++ packages/flutter_file_saver/test/flutter_file_saver_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter_file_saver/flutter_file_saver.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,6 +8,9 @@ import 'utils/mock_file_manager_platform.dart';
 
 void main() {
   group('FlutterFileSaver', () {
+    const tFileName = 'test.txt';
+    const tPath = 'path/to/file/$tFileName';
+
     late FlutterFileSaver fileSaver;
     late MockFileManagerPlatform mockPlatform;
 
@@ -16,27 +21,59 @@ void main() {
       fileSaver.setFileSaverInstance(mockPlatform);
     });
 
-    group('setFileSaverInstance', () {
-      const tFileName = 'test.txt';
+    group('writeFileAsString', () {
       const tData = 'test';
-      const tPath = 'path/to/file/$tFileName';
 
-      test('set platform to MockFileManagerPlatform', () async {
-        // arrange
-        when(
-          () => mockPlatform.writeFileAsString(
+      test(
+        'should call writeFileAsString from the platform implementation',
+        () async {
+          // arrange
+          when(
+            () => mockPlatform.writeFileAsString(
+              fileName: tFileName,
+              data: tData,
+            ),
+          ).thenAnswer((_) async => tPath);
+
+          // act
+          final path = await fileSaver.writeFileAsString(
             fileName: tFileName,
             data: tData,
-          ),
-        ).thenAnswer((_) async => tPath);
+          );
+
+          // assert
+          expect(path, tPath);
+          verify(
+            () => mockPlatform.writeFileAsString(
+              fileName: tFileName,
+              data: tData,
+            ),
+          );
+        },
+      );
+    });
+
+    group('writeFileAsBytes', () {
+      final tBytes = Uint8List.fromList([1, 2, 3, 4, 5]);
+
+      test('should call writeFile from platform implementation', () async {
+        // arrange
+        when(() => mockPlatform.writeFile(fileName: tFileName, bytes: tBytes))
+            .thenAnswer((_) async => tPath);
 
         // act
-        await fileSaver.writeFileAsString(fileName: tFileName, data: tData);
+        final path = await fileSaver.writeFileAsBytes(
+          fileName: tFileName,
+          bytes: tBytes,
+        );
 
         // assert
+        expect(path, tPath);
         verify(
-          () =>
-              mockPlatform.writeFileAsString(fileName: tFileName, data: tData),
+          () => mockPlatform.writeFile(
+            fileName: tFileName,
+            bytes: tBytes,
+          ),
         );
       });
     });
--- packages/flutter_file_saver/test/utils/mock_file_manager_platform.dart
+++ packages/flutter_file_saver/test/utils/mock_file_manager_platform.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_mixin
+
 import 'package:flutter_file_saver/flutter_file_saver.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFileManagerPlatform extends Mock implements FileManagerPlatform {}
+class MockFileManagerPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements FileManagerPlatform {}
