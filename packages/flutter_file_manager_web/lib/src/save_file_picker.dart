96a08e213844fce073c339fb7b7d06cdc1cd1000
Guillaume Roux <rouxguillaume8@gmail.com>
2025-03-28 10:39:52 +0100

    refactor: use JS annotation for safer interop

--- packages/flutter_file_manager_web/lib/src/save_file_picker.dart
+++ packages/flutter_file_manager_web/lib/src/save_file_picker.dart
@@ -1,22 +1,16 @@
 import 'dart:js_interop' as js;
-import 'dart:js_interop_unsafe';
 
-import 'package:web/web.dart' as web;
-
-import 'exceptions.dart';
+@js.JS('window.showSaveFilePicker')
+external js.JSPromise<js.JSAny?> _showSaveFilePicker(js.JSAny? options);
 
 Future<js.JSObject> showSaveFilePicker([
   SaveFilePickerOptions? options,
 ]) async {
-  final fileHandle = await web.window
-      .callMethod<js.JSPromise>(
-        'showSaveFilePicker'.toJS,
-        options?.toJson().jsify(),
-      )
-      .toDart;
+  final fileHandle =
+      await _showSaveFilePicker(options?.toJson().jsify()).toDart;
 
   if (fileHandle == null || !fileHandle.isA<js.JSObject>()) {
-    throw FlutterFileManagerWebException('File handle is null.');
+    throw Exception('File handle is null.');
   }
 
   return fileHandle as js.JSObject;
@@ -30,21 +24,9 @@ class SaveFilePickerOptions {
     this.types,
   });
 
-  /// A boolean value that defaults to false. By default, the picker should
-  /// include an option to not apply any file type filters (instigated with the
-  /// type option below). Setting this option to true means that option is not
-  /// available.
   final bool excludeAcceptAllOption;
-
-  /// By specifying an ID, the browser can remember different directories for
-  /// different IDs. If the same ID is used for another picker, the picker opens
-  /// in the same directory.
   final String? id;
-
-  /// The suggested file name.
   final String? suggestedName;
-
-  /// An Array of allowed file types to save.
   final List<FilePickerType>? types;
 
   Map<String, Object> toJson() {
@@ -64,12 +46,7 @@ class FilePickerType {
     this.description = '',
   }) : assert(accept.length > 0);
 
-  /// An optional description of the category of files types allowed. Default to
-  /// be an empty string.
   final String description;
-
-  /// A map with the keys set to the MIME type and the values an Array of file
-  /// extensions.
   final Map<String, List<String>> accept;
 
   Map<String, Object> toJson() {
