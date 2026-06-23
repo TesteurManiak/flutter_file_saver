29a49e7e59385abd78c386b12a97f56f0e48c6dd
Artemis Kushner <git@arxdeus.dev>
2026-06-24 00:36:55 +0900

    fix: improve import organization in IOSMessageHandler.swift
    (cherry picked from commit a6206a60487dab8fd1abf5b98e2db517068d1b70)

--- packages/flutter_file_manager_ios/ios/flutter_file_manager_ios/Sources/flutter_file_manager_ios/IOSMessageHandler.swift
+++ packages/flutter_file_manager_ios/ios/flutter_file_manager_ios/Sources/flutter_file_manager_ios/IOSMessageHandler.swift
@@ -5,24 +5,25 @@
 //  Created by Guillaume Roux on 24/02/2025.
 //
 
-import Flutter
+import Foundation
 import UIKit
+import Flutter
 
 @available(iOS 16.0, *)
 class IOSMessageHandler : NSObject, IOSMessageApi, UIDocumentPickerDelegate {
     var completionHandler: ((Result<String, any Error>) -> Void)?
-    
+
     func writeFile(fileName: String, bytes: FlutterStandardTypedData, completion: @escaping (Result<String, any Error>) -> Void) {
         guard let viewController = UIApplication.shared.windows.first?.rootViewController else {
             completion(.failure(PigeonError.init(code: "FAILED", message: "No view controller found", details: nil)))
             return
         }
-        
+
         do {
             let tmpUrl = FileManager.default.temporaryDirectory.appending(path: fileName)
             let data = Data(bytes.data)
             try data.write(to: tmpUrl)
-            
+
             let documentPicker = UIDocumentPickerViewController(forExporting: [tmpUrl], asCopy: true)
             documentPicker.delegate = self
             self.completionHandler = completion
@@ -31,7 +32,7 @@ class IOSMessageHandler : NSObject, IOSMessageApi, UIDocumentPickerDelegate {
             completion(.failure(PigeonError.init(code: "FAILED", message: "Error while writing file \(fileName)", details: error.localizedDescription)))
         }
     }
-    
+
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let savedURL = urls.first else {
             completionHandler?(.failure(PigeonError(code: "FAILED", message: "No file selected", details: nil)))
@@ -39,7 +40,7 @@ class IOSMessageHandler : NSObject, IOSMessageApi, UIDocumentPickerDelegate {
         }
         completionHandler?(.success(savedURL.absoluteString))
     }
-    
+
     func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
             completionHandler?(.failure(PigeonError(code: "CANCELLED", message: nil, details: nil)))
         }
