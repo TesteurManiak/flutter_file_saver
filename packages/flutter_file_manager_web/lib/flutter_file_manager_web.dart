f3887bbca81c284ac06054524fa429665b7c4d40
Artemis Kushner <git@arxdeus.dev>
2026-06-23 23:29:47 +0900

    chore: review fixes

--- packages/flutter_file_manager_web/lib/flutter_file_manager_web.dart
+++ packages/flutter_file_manager_web/lib/flutter_file_manager_web.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_runtime_check_with_js_interop_types
+
 library;
 
 import 'dart:js_interop' as js;
@@ -96,7 +98,7 @@ class FlutterFileManagerWeb extends FileManagerPlatform {
       final writable = await fileHandle.createWritable();
       await writable?.write(bytes);
       await writable?.close();
-    } on Object catch (e) {
+    } on web.DOMException catch (e) {
       // Convert an abort error to a custom exception.
       if (e case web.DOMException(code: 20, name: 'AbortError')) {
         throw FileSaverCancelledException();
